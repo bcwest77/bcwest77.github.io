dccc35fa889fb1a470ef84d5ce79f379bcce0906
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 15:31:18 -0600

    z

--- js/main.js
+++ js/main.js
@@ -4,14 +4,21 @@ $(document).ready(function(){
     var activeModal = null;
     $(".board a").keypress(function(e){
         if (e.which == 13) {
-            if (activeModal !== null)
-                $(activeModal).modal("hide");
-
             activeModal = $(this).attr("data-target");
+
+            $(activeModal + " button").focus();
+            $(activeModal + " button").keypress(function(e){
+                if (e.which !== 13) {
+                    e.preventDefault();
+                }
+            });
+
             console.log(activeModal);
             $(this).click();
         }
     });
+
+    $("")
 
     //--------------------------
 
