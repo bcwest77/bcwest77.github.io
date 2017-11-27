b019097d8c4eeefd7a0a2e3aa14d4cc725d7d925
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 17:20:12 -0600

    z

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
             setTimeout(function (){
                 $("#test").focus();
-            }, 1);
+            }, 100);
     
             $(activeModal + " .modal-footer button").keydown(function(e){
                 console.log(1);
