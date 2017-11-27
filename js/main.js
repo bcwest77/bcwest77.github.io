aefd7b3e74792ae5f4a54caac82a301f226a5f29
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 16:29:07 -0600

    z

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
 
     // -----BOARD PAGE ---------
+
+    
+
+    $(".modal-footer button").keypress(function(e){
+        if (e.which !== 13) {
+            e.preventDefault();
+            console.log(1);
+        }
+    });
+
     var activeModal = null;
     $(".board a").keypress(function(e){
         if (e.which == 13) {
             $(this).click();
             activeModal = $(this).attr("data-target");
-
-            $(activeModal).keypress(function(e){
-                if (e.which !== 13) {
-                    e.preventDefault();
-                    console.log(1);
-                }
-            });
 
             console.log(activeModal);
         }
