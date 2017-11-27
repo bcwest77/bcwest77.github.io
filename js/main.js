9ce79b23ba3b4b1bdab59f6f1f7c3a96e4921233
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 16:08:22 -0600

    z

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
             $(this).click();
             activeModal = $(this).attr("data-target");
 
-            console.log(activeModal + ".modal-footer button");
-            $(activeModal + ".modal-footer button").get(0).focus();
-            $(activeModal + ".modal-footer button").get(0).keypress(function(e){
+            console.log(activeModal + " .modal-footer button");
+            $(activeModal + " .modal-footer button").get(0).focus();
+            $(activeModal + " .modal-footer button").get(0).keypress(function(e){
                 if (e.which !== 13) {
                     e.preventDefault();
                     console.log(1);
