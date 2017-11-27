eb39cdb1ca6280014bf6c6598e7d92db2dfee8e4
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 15:02:54 -0600

    Fixing tab behavior on board page

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
 
     // -----BOARD PAGE ---------
-
+    var activeModal = null;
     $(".board a").keypress(function(e){
-        console.log("yo yo");
         if (e.which == 13) {
+            if (activeModal !== null)
+                activeModal = $(this).dataTarget();
+            console.log(activeModal);
             $(this).click();
         }
     });
