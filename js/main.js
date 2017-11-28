6e3752ff28fd049c12ec9076a1e91888470b9da6
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 19:46:41 -0600

    Update modal tab behavior

--- js/main.js
+++ js/main.js
@@ -3,19 +3,32 @@ $(document).ready(function(){
     // -----BOARD PAGE --------
 
     var activeModal = null;
+    // When a user presses a key on a board member
     $(".board a, #committees .col-md-4 a").keydown(function(e){
+        // Check if it's the Return key
         if (e.which == 13) {
+            // If so, click the button
             $(this).click();
+            // Get the modal id from the button clicked
             activeModal = $(this).attr("data-target");
             
-
+            // Set focus to the top button in the modal. A timeout 
+            // is required due to the modal's entrance animation
             setTimeout(function () {
-                $(activeModal + " .modal-footer button").focus();
+                $(activeModal + " .modal-header button").focus();
             }, 150);
 
+            // When a user presses a key on the modal's bottom close button
             $(activeModal + " .modal-footer button").keydown(function(e){
-                if (e.which == 9) {
+                if (e.which == 9 && !e.shiftKey) {
                     e.preventDefault();
+                    $(activeModal + " .modal-header button").focus();
+                }
+            });
+            $(activeModal + " .modal-header button").keydown(function(e){
+                if (e.which == 9 && e.shiftKey) {
+                    e.preventDefault();
+                    $(activeModal + " .modal-footer button").focus();
                 }
             });
         }
