0678ec6ba0b73176dd81b0b5fd042bbd1f07eabf
bwest19 <31266002+bwest690@users.noreply.github.com>
2017-11-27 19:58:54 -0600

    Removed button outlines on info page

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
     var activeModal = null;
     // When a user presses a key on a board member
-    $(".board a, #committees .col-md-4 a").keydown(function(e){
+    $(".board a, #committees-page .col-md-4 a").keydown(function(e){
         // Check if it's the Return key
         if (e.which == 13) {
             // If so, click the button
@@ -20,14 +20,21 @@ $(document).ready(function(){
 
             // When a user presses a key on the modal's bottom close button
             $(activeModal + " .modal-footer button").keydown(function(e){
+                // If the pressed key is only tab, and not tab+shift
                 if (e.which == 9 && !e.shiftKey) {
+                    // Prevent normal tab behavior
                     e.preventDefault();
+                    // Set focus to the modal's top close button
                     $(activeModal + " .modal-header button").focus();
                 }
             });
+            // When a user presses a key on the modal's top close button
             $(activeModal + " .modal-header button").keydown(function(e){
+                // If the pressed key is tab+shift, not just tab
                 if (e.which == 9 && e.shiftKey) {
+                    // Prevent normal tab behavior
                     e.preventDefault();
+                    // Set focus to the modal's buttom close button
                     $(activeModal + " .modal-footer button").focus();
                 }
             });
